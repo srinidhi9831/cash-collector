67a2490feaee1e04e70b3f13fdf523aeec8a3fd9
srinidhi9831 <srinidhi9831@gmail.com>
2021-10-01 13:12:52 +0530

    bug: buf fixed in logic

--- cash.js
+++ cash.js
@@ -1,7 +1,7 @@
 function countNotes(){
 var one=0,five=0,ten=0,twenty=0,hundred=0,fiveHundred=0,twoThousand=0;
-var bill=document.getElementById("bill-amount").value;
-var paid=document.getElementById("amount-paid").value;
+var bill=parseInt(document.getElementById("bill-amount").value);
+var paid=parseInt(document.getElementById("amount-paid").value);
 var change = paid-bill;
 var Ch=change;
 while(change>0)
@@ -22,10 +22,10 @@ while(change>0)
         change=change-100;
         hundred++;
     }
-    if(change>=20&& change<100)
+    if(change>=20 && change<100)
     {
         change=change-20;
-        fiveHundred++;
+        twenty++;
     }
     if(change>=10 && change<20)
     {
@@ -44,5 +44,5 @@ while(change>0)
     }
 
 }
-document.getElementById("output-div").innerText=" change to be returned :" + Ch +"\nDenominations as follows \n 2000 :" + twoThousand + "\n500:" +fiveHundred + "\n100:" + hundred+"\n20:"+twenty+"\n10:"+ten+"\n5:"+five+"\n1:"+one;
+document.getElementById("output-div").innerText=" change to be returned :" + Ch +"\n\nDenominations as follows :\n 2000 :" + twoThousand + "\n500:" +fiveHundred + "\n100:" + hundred+"\n20:"+twenty+"\n10:"+ten+"\n5:"+five+"\n1:"+one;
 }
